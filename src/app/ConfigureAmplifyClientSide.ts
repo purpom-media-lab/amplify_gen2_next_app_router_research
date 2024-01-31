10da5e8dea137b39cbb9f1724d3d6646baa9639c
magisystem0408 <masato.matsudo@gmail.com>
2024-02-01 02:22:42 +0900

    feat: add configure code ssr
    please read
    https://docs.amplify.aws/gen2/build-a-backend/server-side-rendering/#calling-amplify-category-apis-on-the-server-side

---
+++ src/app/ConfigureAmplifyClientSide.ts
@@ -0,0 +1,9 @@
+import {Amplify} from "aws-amplify";
+import config from '../../amplifyconfiguration.json';
+
+Amplify.configure(config, {ssr: true})
+
+export default function ConfigureAmplifyClientSide() {
+    return null;
+}
+
